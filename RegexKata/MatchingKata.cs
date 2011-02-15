daa94b99eaaa56b1b64ef127139b27d8972ac6e0
Sharon Cichelli <sharon@invisible-city.com>
2011-02-15 13:08:14 -0600

    initial checkin

---
+++ RegexKata/MatchingKata.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace RegexKata
+{
+	[TestFixture]
+	public class MatchingKata
+	{
+		private const string SOURCE = "The quick, brown fox jumped 123 times over 12 lazy dogs.";
+
+	}
+}
