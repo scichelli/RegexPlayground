28402d8686c7b73544c84b6d47a92ef987ad8ae1
Sharon Cichelli <sharon@invisible-city.com>
2011-02-20 20:53:27 -0600

    setting up framework of kata unit tests

--- RegexKata/MatchingKata.cs
+++ RegexKata/MatchingKata.cs
@@ -1,11 +1,44 @@
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace RegexKata
 {
+	/// <summary>
+	/// This kata is a series of exercises in defining regular expression patterns to match given inputs.
+	/// Complete each exercise by defining a pattern that passes all of the TestCases. You might also need 
+	/// to supply RegexOptions to the Regex constructor.
+	/// </summary>
 	[TestFixture]
 	public class MatchingKata
 	{
-		private const string SOURCE = "The quick, brown fox jumped 123 times over 12 lazy dogs.";
+		[TestCase("car", Result = true)]
+		[TestCase("Car", Result = false)]
+		[TestCase("CaR", Result = false)]
+		[TestCase("CAR", Result = false)]
+		[TestCase("carabiner", Result = true)]
+		[TestCase("scare", Result = true)]
+		[TestCase("cae", Result = false)]
+		[TestCase("caar", Result = false)]
+		public bool LiteralCharacters_CaseSensitive_WithinWord(string input)
+		{
+			const string pattern = "";
+			var regex = new Regex(pattern);
+			return regex.IsMatch(input);
+		}
 
+		[TestCase("car", Result=true)]
+		[TestCase("Car", Result=true)]
+		[TestCase("CaR", Result=true)]
+		[TestCase("CAR", Result=true)]
+		[TestCase("carabiner", Result=true)]
+		[TestCase("scare", Result=true)]
+		[TestCase("cae", Result = false)]
+		[TestCase("caar", Result = false)]
+		public bool LiteralCharacters_CaseInsensitive_WithinWord(string input)
+		{
+			const string pattern = "";
+			var regex = new Regex(pattern);
+			return regex.IsMatch(input);
+		}
 	}
 }
