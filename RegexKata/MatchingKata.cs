12f95e144311662973cc88f87f33fb654afd8125
Sharon Cichelli <sharon@invisible-city.com>
2011-02-20 21:09:07 -0600

    adding exercises, notes to help when publishing, answers to exercises

--- RegexKata/MatchingKata.cs
+++ RegexKata/MatchingKata.cs
@@ -8,6 +8,15 @@ namespace RegexKata
 	/// Complete each exercise by defining a pattern that passes all of the TestCases. You might also need 
 	/// to supply RegexOptions to the Regex constructor.
 	/// </summary>
+	/* 
+	Notes to self, to prep for blog post:
+	 1. To run tests, run C:\Users\Sharon\CodeUnruly\NUnitRunner\NUnit-2.5.5.10112\bin\net-2.0\nunit.exe, 
+	 and open the RegexKata.nunit project in this solution's bin\Debug folder.
+	 2. To clear the answers from the tests, use a Regex find-replace. 
+	 Find: const string pattern = \".*\";
+	 Replace with: const string pattern = \"\";
+	 3. Delete this comment, but keep the summary.
+	*/
 	[TestFixture]
 	public class MatchingKata
 	{
@@ -21,22 +30,36 @@ namespace RegexKata
 		[TestCase("caar", Result = false)]
 		public bool LiteralCharacters_CaseSensitive_WithinWord(string input)
 		{
-			const string pattern = "";
+			const string pattern = "car";
 			var regex = new Regex(pattern);
 			return regex.IsMatch(input);
 		}
 
-		[TestCase("car", Result=true)]
-		[TestCase("Car", Result=true)]
-		[TestCase("CaR", Result=true)]
-		[TestCase("CAR", Result=true)]
-		[TestCase("carabiner", Result=true)]
-		[TestCase("scare", Result=true)]
+		[TestCase("car", Result = true)]
+		[TestCase("Car", Result = true)]
+		[TestCase("CaR", Result = true)]
+		[TestCase("CAR", Result = true)]
+		[TestCase("carabiner", Result = true)]
+		[TestCase("scare", Result = true)]
 		[TestCase("cae", Result = false)]
 		[TestCase("caar", Result = false)]
 		public bool LiteralCharacters_CaseInsensitive_WithinWord(string input)
 		{
-			const string pattern = "";
+			const string pattern = "[Cc][Aa][Rr]";
+			var regex = new Regex(pattern);
+			return regex.IsMatch(input);
+		}
+
+		[TestCase("car", Result = true)]
+		[TestCase("a car parks.", Result = true)]
+		[TestCase("a car.", Result = true)]
+		[TestCase("a caR.", Result = true)]
+		[TestCase("scare", Result = false)]
+		[TestCase("cargo", Result = false)]
+		[TestCase("gocar", Result = false)]
+		public bool LiteralCharacters_CaseInsensitive_AsAWord(string input)
+		{
+			const string pattern = @"\b[Cc][Aa][Rr]\b";
 			var regex = new Regex(pattern);
 			return regex.IsMatch(input);
 		}
